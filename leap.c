f48006b78569862080bfc330242c92942991f403
Rockers Mukesh <110319782+rockersmukesh@users.noreply.github.com>
2023-11-24 19:05:24 +0530

    Add files via upload
    check leap or not

---
+++ leap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int year;
+	printf("Enter the year:");
+	scanf("%d",&year);
+	if(year%4==0 && (year%100!=0 || year%400==0))
+	{
+		printf("It is leap year");
+	}
+	else
+	{
+		printf("It is not leap year");
+	}
+}
