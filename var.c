dc565b1dcdfb1902d60fed7124ce61336cfbf259
Rockers Mukesh <110319782+rockersmukesh@users.noreply.github.com>
2023-12-07 23:15:19 +0530

    Create var.c
    Input integer value from user

---
+++ var.c
@@ -0,0 +1,12 @@
+// Take 1 number input from user and show on screen
+
+#include<stdio.h>
+int main()
+{
+    int num; // Variable declaration
+
+    printf("Please enter a number:");
+    scanf("%d", &num);
+
+    printf("You entered value = %d\n", num);
+}
